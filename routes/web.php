e39ae8f9ebdc0df0f46eba67980f0a86455f2b91
berlybeer <b.pumacajia.cz@gmail.com>
2020-09-24 22:11:32 -0500

    Regular expression route constraints

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,30 @@ use App\Greeting;
 Route::get('/', "WelcomeController@index");
 
 
-Route::get('users/{id}/friends', function($id){
+Route::get('users/{id}', function($id){
   return $id;
-});
+})->where('id', '[0-9]+');
 
 
-//You should also provide a default value for the route's corresponding variable
-Route::get('users/{id?}', function($id = 'fallbackId'){
- return $id;
+Route::get('login/{username}', function($username){
+  return $username;
+})->where('username', '[A-Za-z]+');
+
+Route::get('posts/{id}/{slug}', function($id, $slug){
+ $mix = $id . $slug;
+ return $mix;
+})->where(['id'=> '[0-9]+', 'slug'=>'[A-Za-z]+']);
+
+
+//The only thing that defines which route parameter matches with 
+//method parameter is their order(left to right), as you can see here:
+Route::get('users/{userId}/comments/{commentId}', function(
+  $thisIsActuallyTheUserId,
+  $thisIsReallyTheCommentId
+){
+
+
+  $mix = $thisIsActuallyTheUserId . $thisIsReallyTheCommentId;
+  return $mix;
+  
 });
